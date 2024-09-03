b3a733a5da28dfe1bf2d1eb2f2e8427e6d866607
Syed Imran <syedimran.dev1@gmail.com>
2024-09-03 23:02:01 +0500

    frontend show tool slug, parent, and child tools

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,13 @@ use App\Http\Controllers\ToolsController;
 
 
 
-Route::get('/{slug?}', [FrontendController::class, 'showPageBySlug'])
-    ->where('slug', '[a-zA-Z0-9-]+')
-    ->name('page.get');
+Route::get('/', [FrontendController::class, 'homeTool'])->name('home');
+
+// Route for tools without language parameter (e.g., /tool-slug)
+Route::get('/{slug}', [FrontendController::class, 'native_tools_language'])->name('tool');
+
+// Route for tools with language parameter (e.g., /es/tool-slug)
+Route::get('{lang}/{slug}', [FrontendController::class, 'other_tools_language'])->where(['lang' => '[a-z]{2}', 'slug' => '[\w-]+'])->name('tool.lang');
+
+// Route for custom pages directly with {slug}
+// Route::get('{slug}', [FrontendController::class, 'showCustomPageBySlug'])->name('custom.page');
