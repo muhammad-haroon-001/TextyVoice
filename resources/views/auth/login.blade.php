d55dec2e9964aeb8f7bab9fadba8a199dfc53c0b
Muhammad Haroon <97999682+muhammad-haroon-001@users.noreply.github.com>
2024-09-08 19:23:29 +0500

    title update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,20 +1,31 @@
-<link rel="stylesheet" href="{{ asset('assets/admin/css/admin.css')}}">
-<div class="login-page">
-    <div class="form">
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-            <div>
-                <input type="email" name="email" placeholder="Email" value="{{ old('email') }}" required id="email">
-            </div>
-            <div>
-                <input type="password" name="password" placeholder="Password" value="{{ old('password') }}" required id="password">
-            </div>
-            <div class="message">
-                <a href="{{ route('register') }}">don't have an account</a>
-            </div>
-            <div class="mt-5">
-                <button type="submit" class="btn btn-primary waves-effect waves-light">Sign in</button>
-            </div>
-        </form>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login|EMD</title>
+    <link rel="stylesheet" href="{{ asset('assets/admin/css/admin.css')}}">
+</head>
+<body>
+    <div class="login-page">
+        <div class="form">
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+                <div>
+                    <input type="email" name="email" placeholder="Email" value="{{ old('email') }}" required id="email">
+                </div>
+                <div>
+                    <input type="password" name="password" placeholder="Password" value="{{ old('password') }}" required id="password">
+                </div>
+                <div class="message">
+                    <a href="{{ route('register') }}">don't have an account</a>
+                </div>
+                <div class="mt-5">
+                    <button type="submit" class="btn btn-primary waves-effect waves-light">Sign in</button>
+                </div>
+            </form>
+        </div>
     </div>
-</div>
+</body>
+</html>
