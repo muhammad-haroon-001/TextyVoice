ed0b8e67335fb3e72c07d7c8bee749b67637c8db
Muhammad Haroon <97999682+muhammad-haroon-001@users.noreply.github.com>
2024-09-10 20:40:11 +0500

    blog & gitignore

---
+++ resources/views/frontend/emd_blog_pages/blog.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('content')
+   <div class="container">
+    <div class="bg-red py-5">
+        <h1>blog page</h1>
+    </div>
+   </div>
+@endsection
---
+++ resources/views/frontend/emd_blog_pages/single_blog.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('content')
+   <div class="container">
+    <div class="bg-red py-5">
+        <h1>single blog page</h1>
+    </div>
+   </div>
+@endsection
