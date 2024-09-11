67dc8f4d9248fc39e0856d5e5a67f44a14b2d444
Syed Imran <syedimran.dev1@gmail.com>
2024-09-11 20:25:28 +0500

    changes

--- routes/custom_pages.php
+++ routes/custom_pages.php
@@ -6,6 +6,5 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::controller(EmdCustomPageDisplayController::class)->group(function () {
-    Route::get('contact-us', 'custom_page_display')->name('EmdCustomPage.contact-us');
-    Route::get('terms-of-services', 'custom_page_display')->name('EmdCustomPage.terms-of-services');
+  Route::get('contact', 'custom_page_display')->name('EmdCustomPage.contact');
 });
