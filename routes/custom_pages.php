1984e144e2f1c16b1e53492965fe7564412d234e
Muhammad Haroon <97999682+muhammad-haroon-001@users.noreply.github.com>
2024-09-17 17:57:19 +0500

    script links & custom page route

--- routes/custom_pages.php
+++ routes/custom_pages.php
@@ -6,5 +6,5 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::controller(EmdCustomPageDisplayController::class)->group(function () {
-  //
+  Route::get('contact-us', 'custom_page_display')->name('EmdCustomPage.contact-us');
 });
