527126f18d7de6f25528a635c6a2bc7a872dbc51
syedimran <syedimran.dev1@gmail.com>
2024-09-18 21:27:49 +0500

    blog fix

--- resources/views/frontend/emd_blog_pages/blog.blade.php
+++ resources/views/frontend/emd_blog_pages/blog.blade.php
@@ -5,8 +5,10 @@
     <div class="bg-red py-5">
         <h1>blog page</h1>
         @foreach ($blogs as $item)
-            <h3>{{ $item->title }}</h3>
-            <a href="{{ route('single.blog', $item->slug) }}" class="text-white">Readmore</a>
+        @dd($item);
+            {{-- <h3>{{ $item->title }}</h3> --}}
+            <img src="{{asset()}}" alt="">
+            {{-- <a href="{{ route('single.blog', $item->slug) }}" class="text-white">Readmore</a> --}}
         @endforeach
     </div>
    </div>
