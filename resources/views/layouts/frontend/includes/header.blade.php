3544ff63b5f584f6bc01e358d55fbb6ccc2cef12
Muhammad Haroon <97999682+muhammad-haroon-001@users.noreply.github.com>
2024-09-26 20:00:54 +0500

    header design fixes

--- resources/views/layouts/frontend/includes/header.blade.php
+++ resources/views/layouts/frontend/includes/header.blade.php
@@ -6,7 +6,7 @@
                     <img src="{{ asset('assets/frontend/image/logo.svg') }}" alt="logo" />
                 </div>
                 <div>
-                    <p class="logo-text nav-list-active">Texty Audio</p>
+                    <p class="logo-text nav-list-active">Texty Voice</p>
                 </div>
             </a>
         </div>
@@ -51,18 +51,45 @@
                         <img src="{{ asset('assets/frontend/image/logo.svg') }}" alt="logo" />
                     </div>
                     <div>
-                        <p class="logo-text nav-list-active">Texty Audio</p>
+                        <p class="logo-text nav-list-active">Texty Voice</p>
                     </div>
                 </a>
             </div>
             <div class="toogle_header">
+                <div id="language-mobile" class="language-switcher">
+                    <span class="name" id="selected-language">English</span>
+                    <img src="{{ asset('assets/frontend/image/language_drop.svg') }}" alt="language">
+                    <ul class="language-dropdown">
+                        <li data-lang="en">
+                            <img src="{{ asset('assets/frontend/image/en.svg') }}" alt="en">
+                            <span>English</span>
+                        </li>
+                        <li data-lang="es">
+                            <img src="{{ asset('assets/frontend/image/es.svg') }}" alt="es">
+                            <span>Spanish</span>
+                        </li>
+                        <li data-lang="de">
+                            <img src="{{ asset('assets/frontend/image/de.svg') }}" alt="de">
+                            <span>German</span>
+                        </li>
+                        <li data-lang="ja">
+                            <img src="{{ asset('assets/frontend/image/ja.svg') }}" alt="ja">
+                            <span>Japanese</span>
+                        </li>
+                        <li data-lang="fr">
+                            <img src="{{ asset('assets/frontend/image/fr.svg') }}" alt="fr">
+                            <span>French</span>
+                        </li>
+                    </ul>
+                </div>
                 <img id="dropdown_bar" src="{{ asset('assets/frontend/image/dropdown_bar.svg') }}" alt="toogle" />
             </div>
         </div>
         <div class="mobile-dropdown">
-            <a href="{{ route('home') }}">Home</a>
-            <a href="{{ route('blog') }}">Blogs</a>
-            <a href="{{ route('EmdCustomPage.contact-us') }}">Contact</a>
+            <a href="{{ route('home') }}">Text To Speech</a>
+            <a href="{{ route('tool', 'speech-to-text') }}">Speech To Text</a>
+            <a href="{{ route('blog') }}" class="">Blogs</a>
+            <a href="{{ route('EmdCustomPage.contact-us') }}" class="">Contact</a>
         </div>
     </div>
 </nav>
